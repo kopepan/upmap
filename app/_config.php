6006feb9bbdc79824cafdc085494d221dd9ef9da
kopepan <koko5688287@gmail.com>
2020-01-12 14:43:31 +0700

    add app config

---
+++ app/_config.php
@@ -0,0 +1,10 @@
+<?php
+  define("RESOURCE_PATH", "resources");
+  define("COMPONENT_PATH", RESOURCE_PATH."/components");
+  define("SCRIPT_PATH", RESOURCE_PATH."/scripts");
+  define("STYLE_PATH", RESOURCE_PATH."/styles");
+  define("PLUGIN_PATH", RESOURCE_PATH."/plugins");
+  define("IMG_PATH", RESOURCE_PATH."/imgs");
+  define("APP_URL", "/upmap/app");
+  define("API_URL", "/upmap/api");
+?>
--- app/index.php
+++ app/index.php
@@ -1,16 +1,26 @@
+<?php 
+	include_once('_config.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<title>UP Map</title>
-  <link rel="stylesheet" type="text/css" href="resources/plugins/bootstrap/css/bootstrap.min.css?<?= rand(1, 100000)?>">
-  <link rel="stylesheet" type="text/css" href="resources/plugins/leaflet/leaflet.css?<?= rand(1, 100000)?>"/>
-	<link rel="stylesheet" type="text/css" href="resources/styles/index.css?<?= rand(1, 100000)?>">
+  <link rel="stylesheet" type="text/css" href="<?=PLUGIN_PATH?>/bootstrap/css/bootstrap.min.css?<?= rand(1, 100000)?>">
+  <link rel="stylesheet" type="text/css" href="<?=PLUGIN_PATH?>/leaflet/leaflet.css?<?= rand(1, 100000)?>"/>
+	<link rel="stylesheet" type="text/css" href="<?=STYLE_PATH?>/index.css?<?= rand(1, 100000)?>">
 </head>
 	<body>
-		<?php include('resources/components/loader.php'); ?>
+		<?php include(COMPONENT_PATH.'/loader.php'); ?>
+		<div>
+			<div class="header">
+				<div>
+					<img src="<?=IMG_PATH?>/logo.png" alt="UP map logo">
+				</div>
+			</div>
+		</div>
 	</body>
-	<script type="text/javascript" src="resources/plugins/jquery/jquery-3.4.1.min.js?<?= rand(1, 100000)?>"></script>
-	<script type="text/javascript" src="resources/plugins/leaflet/leaflet.js?<?= rand(1, 100000)?>" defer></script>
-  <script type="module" src="resources/scripts/pages/home_page.js?<?= rand(1, 100000)?>"></script>
+	<script type="text/javascript" src="<?=PLUGIN_PATH?>/jquery/jquery-3.4.1.min.js?<?= rand(1, 100000)?>"></script>
+	<script type="text/javascript" src="<?=PLUGIN_PATH?>/leaflet/leaflet.js?<?= rand(1, 100000)?>" defer></script>
+  <script type="module" src="<?=SCRIPT_PATH?>/pages/home_page.js?<?= rand(1, 100000)?>"></script>
 </html>
