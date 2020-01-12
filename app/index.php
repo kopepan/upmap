e1ebc7b4f244711638da78caad0e86bdf71bfb12
kopepan <koko5688287@gmail.com>
2020-01-12 14:05:32 +0700

    open loader + change script path to scripts

--- app/index.php
+++ app/index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" type="text/css" href="resources/styles/index.css?<?= rand(1, 100000)?>">
 </head>
 	<body>
-		<div class="loader" style="display:none;">
+		<div class="loader">
 			<div class="loading-text">กำลังโหลด</div>
 			<div class="spinner">
 				<div class="bubble-1"></div>
@@ -18,6 +18,6 @@
 	</body>
 	<script type="text/javascript" src="resources/plugins/jquery/jquery-3.4.1.min.js?<?= rand(1, 100000)?>"></script>
 	<script type="text/javascript" src="resources/plugins/leaflet/leaflet.js?<?= rand(1, 100000)?>" defer></script>
-  <script type="text/javascript" src="resources/script/index.js?<?= rand(1, 100000)?>"></script>
-  <script type="text/javascript" src="resources/script/pages/home_page.js?<?= rand(1, 100000)?>"></script>
+  <script type="text/javascript" src="resources/scripts/index.js?<?= rand(1, 100000)?>"></script>
+  <script type="text/javascript" src="resources/scripts/pages/home_page.js?<?= rand(1, 100000)?>"></script>
 </html>
