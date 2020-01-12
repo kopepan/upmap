c55c2ffdfbfdfbe5690bf3ad63e2ced2335b289b
kopepan <koko5688287@gmail.com>
2020-01-12 14:00:36 +0700

    init frontend structure

---
+++ app/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>UP Map</title>
+  <link rel="stylesheet" type="text/css" href="resources/plugins/bootstrap/css/bootstrap.min.css?<?= rand(1, 100000)?>">
+  <link rel="stylesheet" type="text/css" href="resources/plugins/leaflet/leaflet.css?<?= rand(1, 100000)?>"/>
+	<link rel="stylesheet" type="text/css" href="resources/styles/index.css?<?= rand(1, 100000)?>">
+</head>
+	<body>
+		<div class="loader" style="display:none;">
+			<div class="loading-text">กำลังโหลด</div>
+			<div class="spinner">
+				<div class="bubble-1"></div>
+				<div class="bubble-2"></div>
+			</div>
+    </div>
+	</body>
+	<script type="text/javascript" src="resources/plugins/jquery/jquery-3.4.1.min.js?<?= rand(1, 100000)?>"></script>
+	<script type="text/javascript" src="resources/plugins/leaflet/leaflet.js?<?= rand(1, 100000)?>" defer></script>
+  <script type="text/javascript" src="resources/script/index.js?<?= rand(1, 100000)?>"></script>
+  <script type="text/javascript" src="resources/script/pages/home_page.js?<?= rand(1, 100000)?>"></script>
+</html>
